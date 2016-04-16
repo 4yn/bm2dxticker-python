981a095d28a5a6e266c999cf9afc8c93bf715490
Chang Si Yuan <changsiyuan1999@gmail.com>
2016-04-16 22:55:15 +0800

    Clean up code, add bm2dx dll selector

--- server/ticker.py
+++ server/ticker.py
@@ -1,72 +1,68 @@
 from ctypes import *
 from ctypes.wintypes import *
-import win32gui,win32process
+import json
 import socket
-import time
-import os.path
+import win32gui,win32process
 from websocket_server import WebsocketServer
 
+# alias to functions
 OpenProcess = windll.kernel32.OpenProcess
 ReadProcessMemory = windll.kernel32.ReadProcessMemory
 CloseHandle = windll.kernel32.CloseHandle
+GetWindowPid = win32process.GetWindowThreadProcessId
+EnumProcessModules = win32process.EnumProcessModules
+GetModuleName = win32process.GetModuleFileNameEx
 
-PROCESS_ALL_ACCESS = 0x1F0FFF
-
-memory_address = 0x1143AE30
-# memory_address = 0x0384AE30
-
+# variables
+client_num = 0
+memory_address = 0
+phandle = 0
 ticker = c_char_p("*********")
 ticker_s = len(ticker.value)
 bytesRead = c_ulong(0)
 
-num_clients = 0
+# handle game data
+config = json.load(open('config.txt'))
+for g in config["gameList"]:
+	if config["currentGame"] == g["gameIndex"]:
+		game = g
 
-phandle = 0
+# create handle, define address
+def initTicker():
+	global phandle, memory_address
+	CloseHandle(phandle)
+	hwnd  = win32gui.FindWindow(None, game["windowTitle"])
+	threadid,pid = GetWindowPid(hwnd)
+	phandle = OpenProcess(0x1F0FFF,False,pid)
+	modlist = EnumProcessModules(phandle)
+	for mod in modlist:
+		mname = GetModuleName(phandle,mod)
+		if mname.endswith("bm2dx.dll"):
+			memory_address = mod + game["memoryOffset"]
 
-server_port=9001
-
-server_port = int(raw_input())
-memory_address = int(raw_input()[2:],16)
-
-print "Targeting address: 0x" + hex(memory_address)
-
-def get_window_pid(title):
-    hwnd = win32gui.FindWindow(None, title)
-    threadid,p_id = win32process.GetWindowThreadProcessId(hwnd)
-    return p_id
-
-def setup_process():
-	global phandle
-	pid = get_window_pid("beatmania IIDX 22 PENDUAL")
-	phandle = OpenProcess(PROCESS_ALL_ACCESS,False,pid)
-
-def read_process():
+# read address
+def readTicker():
 	ReadProcessMemory(phandle, memory_address, ticker, ticker_s, byref(bytesRead))
 
-def close_process():
-	CloseHandle(phandle)
+def clientJoin(client,server):
+	global client_num
+	client_num += 1
+	if(client_num==1):
+		initTicker()
 
-def new_client(client, server):
-	global num_clients 
-	num_clients += 1
-	if(num_clients==1):
-		setup_process()
-	server.send_message_to_all("CONNECT ")
+def clientLeave(client,server):
+	global client_num
+	client_num -= 1
+	if(client_num==0):
+		CloseHandle(phandle)
 
-def client_left(client, server):
-	global num_clients
-	num_clients -= 1
-	if(num_clients==0):
-		close_process()
-
-def message_received(client, server, message):
-	read_process()
-	server.send_message_to_all(ticker.value.replace('m','.'))
+def clientMsg(client,server,message):
+	readTicker()
+	server.send_message(client,ticker.value.replace('m','.'))
 
 print "Starting server on " + str(socket.gethostbyname(socket.gethostname()))
-
-server = WebsocketServer(server_port,"0.0.0.0")
-server.set_fn_new_client(new_client)
-server.set_fn_client_left(client_left)
-server.set_fn_message_received(message_received)
+server = WebsocketServer(config["serverPort"],"0.0.0.0")
+server.set_fn_new_client(clientJoin)
+server.set_fn_client_left(clientLeave)
+server.set_fn_message_received(clientMsg)
 server.run_forever()
